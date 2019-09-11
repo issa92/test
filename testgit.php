3049bc25c8980788fcec9e614df046c3bb4f9aa5
Issa N'diaye <issalvaro9@gmail.com>
2019-09-11 10:36:02 +0200

    test

---
+++ testgit.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    Le cœur est morose, et seul le lean est rose <br>
+    Rien n'va changer, même si j'augmente la dose <br>
+    Avant qu'explose la Terre, la stratégie j'expose <br>
+    On t'crée la névrose pour que t'augmentes la dose <br>
+    Un jour, tu oseras relever la tête <br>
+    Pour l'instant, la porte est close, donc augmente la dose <br>
+    "Megadose versée par terre pour les innocents qu'on arrose" <br>
+    Le marché nous a baisé, j'regarde les frères se faire rabaisser <br>
+    Pour un salaire se faire enlaisser, fermer sa gueule, ne faire qu'encaisser <br>
+    S'empresser d's'engraisser sans précédent <br>
+    La colombe est amiantée, nos addictions sont manigancées <br>
+    La frustration est alimentée, comment patienter? <br>
+    J'arrive en balle comme un trafiquant d'armes et, comme lui, j'irai partir en paix <br>
+    </p>
+</body>
+</html>
